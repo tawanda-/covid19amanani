b1b705ff9ab3f853e162a7ff6b7752e791d0f95c
Tawanda <tmuhwati@gmail.com>
2022-06-28 18:09:00 +0200

    added db

--- backend/api/db.py
+++ backend/api/db.py
@@ -4,7 +4,12 @@
 '''
 
 import psycopg
+import click
 from flask import current_app, g
+from flask.cli import with_appcontext
+
+COPY_COUNTRY = "COPY country (iso,country,population,life_expectancy,continent,location,capital_city) FROM STDIN"
+COPY_CURRENT_DATA = "COPY current_data (iso, confirmed, deaths, administered, people_vaccinated, people_partially_vaccinated) FROM STDIN"
 
 def get_db(autocommit=False):
     if 'db' not in g:
@@ -22,3 +27,28 @@ def close_db(e=None):
 
     if db is not None:
         db.close()
+
+'''
+    Add tables to DB
+'''
+def init_db():
+    db = get_db()
+
+    with current_app.open_resource('schema.sql') as f:
+        cursr = db.cursor()
+        cursr.execute(f.read().decode('utf8'))
+        db.commit()
+        cursr.close()
+
+'''
+    Cmd line method to initialise db
+'''
+@click.command('init-db')
+@with_appcontext
+def init_db_command():
+    init_db()
+    click.echo('Database initialised.')
+
+def init_app(app):
+    app.teardown_appcontext(close_db)
+    app.cli.add_command(init_db_command)
